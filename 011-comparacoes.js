68f8961b851a78e853b04d17c6199625f184864d
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-09-08 20:40:01 -0300

    aula 08-09

---
+++ 011-comparacoes.js
@@ -0,0 +1,20 @@
+
+let valor1 = 5; 
+let valor2 = 3; 
+let valor3 = 1; 
+let valor4 = 3; 
+let valor5 = 'Senha'; 
+let valor6 = 'senha'; 
+let valor7 = '';
+
+let maior = valor1 > valor2;
+let menor = valor1 < valor2;
+let maiorIgual = valor2 >= valor4;
+let menorIgual = valor2 <= valor4;
+
+let igual = valor5 == valor6;
+let igualMesmo = valor5 === valor6;
+let diferente = valor5 != valor6;
+let diferenteMesmo = valor5 !== valor6;
+
+// not
